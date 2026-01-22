ef49f9bdd9b1d770609e3773937f3800641c782a
Bimonrchyo <mohammad.bimo.nurcahyo@gmail.com>
2026-01-22 14:32:00 +0700

    migrasi database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,36 +4,83 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        // User Tabel
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->enum('role', ['superAdmin', 'admin', 'user']);
+            $table->timestamp('created_at');
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        //Transportasi Tabel
+        Schema::create('transportasi', function (Blueprint $table) {
+            $table->id();
+            $table->enum('tipe', ['pesawat', 'bus', 'kereta', 'kapal']);
+            $table->string('nama_brand');
+            $table->string('kode_identitas');
+            $table->integer('kapasitas');
+            $table->integer('admin_id')
+                ->foreignId('user_id')
+                ->constrained('user')
+                ->cascadeOnDelete();
+
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+        //Jadwal Tabel
+        Schema::create('jadwal', function (Blueprint $table) {
+            $table->id();
+            $table->integer('transportasi_id')
+                ->foreignId('transportasi_id')
+                ->constrained('transportasi')
+                ->cascadeOnDelete();
+            $table->string('titik_asal');
+            $table->string('titik_tujuan');
+            $table->dateTime('waktu_berangkat');
+            $table->dateTime('waktu_tiba');
+            $table->decimal(15, 2);
+            $table->string('info_lokasi');
+            $table->integer('stok_tersedia');
+        });
+
+        //Pemesanan Tabel
+        Schema::create('pemesanan', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_booking')->unique();
+            $table->integer('user_id')
+                ->foreignId('user_id')
+                ->constrained('user')
+                ->cascadeOnDelete();
+            $table->integer('jadwal_id')
+                ->foreignId('jadwal_id')
+                ->constrained('jadwal')
+                ->cascadeOnDelete();
+            $table->string('nomor_kursi');
+            $table->decimal(15, 2); //total harga 
+            $table->enum('status', ['pending', 'paid', 'canceled', 'completed']);
+            $table->text('qr_code_data');
+            $table->timestamp('created_at');
+        });
+
+        //Pembayaran Tabel
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->id();
+            $table->integer('booking_id')
+                ->foreignId('pemesanan_id')
+                ->constrained('pemesanan')
+                ->cascadeOnDelete();
+            $table->string('metode_bayar');
+            $table->string('bukti_transfer');
+            $table->decimal(15, 2, ); //nominal bayar
+            $table->timestamp('payment_time');
+            $table->timestamp('verified_at');
         });
     }
 
@@ -42,8 +89,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('user');
+        Schema::dropIfExists('transportasi');
+        Schema::dropIfExists('jadwal');
+        Schema::dropIfExists('pemesanan');
+        Schema::dropIfExists('pembayaran');
     }
 };
