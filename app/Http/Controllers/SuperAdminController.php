72bc9fa10e85c61e2f030175b4478fb940d32147
arfabanyu <banyuarfa1122@gmail.com>
2026-03-29 11:42:04 +0700

    >> bbrp bersih bersih

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Booking;
 use App\Models\Company;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class SuperAdminController extends Controller
